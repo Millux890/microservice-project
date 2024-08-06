7eb6a6ed116f30aabfd3b3ca3e1331ae9b567d08
millux890 <tdmz@o2.pl>
2024-08-06 21:21:13 +0200

    Creating discovery server + fix other services

--- order-service/src/main/java/com/brozekdev/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/brozekdev/orderservice/service/OrderService.java
@@ -2,11 +2,13 @@ package com.brozekdev.orderservice.service;
 
 import com.brozekdev.orderservice.dto.OrderLineItemDto;
 import com.brozekdev.orderservice.dto.OrderRequest;
+import com.brozekdev.orderservice.dto.ProductResponse;
 import com.brozekdev.orderservice.model.Order;
 import com.brozekdev.orderservice.model.OrderLineItem;
 import com.brozekdev.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.UUID;
 
@@ -16,16 +18,29 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
-    public void placeOrder(OrderRequest orderRequest){
+    private final WebClient.Builder webClient;
+
+    public void placeOrder(OrderRequest orderRequest) {
         Order order = Order.builder()
                 .orderNumber(UUID.randomUUID().toString())
                 .orderLineItemList(orderRequest.getOrderLineItemList().stream().map(this::mapToDto).toList())
                 .build();
+        String productName = "samsung";
 
-        orderRepository.save(order);
+        ProductResponse result = webClient.build().get()
+                .uri("http://product-service/product-service/products/product/" + productName)
+                .retrieve()
+                .bodyToMono(ProductResponse.class)
+                .block();
+        if (result != null) {
+            System.out.println(result);
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("stop");
+        }
     }
 
-    private OrderLineItem mapToDto(OrderLineItemDto orderLineItemDto){
+    private OrderLineItem mapToDto(OrderLineItemDto orderLineItemDto) {
         OrderLineItem orderLineItem = new OrderLineItem();
         orderLineItem.setPrice(orderLineItemDto.getPrice());
         orderLineItem.setQuantity(orderLineItemDto.getQuantity());
